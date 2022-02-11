3d1e55d8307fc64cd5742f2e0eb02fd52a305d70
Maximilian van Laar <m.vanlaar@vistacollege.nl>
2022-02-11 13:02:52 +0100

    Opzet structuur LVS systeem_Low prototype

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LVS
  Leerlingvolgsysteem
+
+ 
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include_once 'includes/connect.php';
+include 'includes/header.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+include 'includes/footer.php';
